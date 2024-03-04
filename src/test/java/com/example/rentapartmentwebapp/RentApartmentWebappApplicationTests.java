eb90a896a676e135d7b94429981176aa17592399
hieu15402 <100761394+hieu15402@users.noreply.github.com>
2024-03-04 16:08:53 +0700

    start-project

---
+++ src/test/java/com/example/rentapartmentwebapp/RentApartmentWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rentapartmentwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentApartmentWebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
